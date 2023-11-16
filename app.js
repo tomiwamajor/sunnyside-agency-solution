a0880577f491c191fa16e704f4867a73201feba7
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-11-16 11:44:32 +0100

    new changes

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 "strict";
+const hamburger = document.querySelector(".ham-icon");
+const toggle = document.querySelector(".toggle");
+const links = document.querySelector(".links");
+
+hamburger.addEventListener("click", () => links.classList.toggle("toggle"));
